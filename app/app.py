ee879c11ed766e0cfc34b52e8cbdf16b77bbd471
Zack Adams <imzackadams@protonmail.com>
2023-10-18 06:52:18 -0400

    create chatbot route

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return open('index.html').read()
+
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot_response():
+    user_query = request.json['query']
+    # For now, we'll just echo back the query.
+    # In a real-world scenario, you'd pass this to your NLP model or logic for processing.
+    return jsonify({"response": f"You said: {user_query}"})
 
 
 if __name__ == '__main__':
