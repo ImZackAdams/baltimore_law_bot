eb434a7845fb9399220627e9a3525da906909622
Zack Adams <imzackadams@protonmail.com>
2023-10-18 06:17:41 -0400

    delete app directory and run.py

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .views import views
-
-# Initialize the Flask app
-app = Flask(__name__)
-
-# Register the blueprint
-app.register_blueprint(views)
--- app/shared.py
+++
@@ -1,7 +0,0 @@
-from app.utils.pdf_processing import extract_text_from_pdf, clean_text
-
-# Path to your PDF file
-pdf_path = "legaldocs/Article-13-housing.pdf"
-
-# Extract and clean the text from the PDF
-main_law_text = clean_text(extract_text_from_pdf(pdf_path))
